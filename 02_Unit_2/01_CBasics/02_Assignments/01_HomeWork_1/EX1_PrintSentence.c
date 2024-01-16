b481269343e3aa6ce787f7edda5d37b6f35f837c
0xmuhammedalii99 <muhammed9alii9@gmail.com>
2024-01-16 20:22:54 +0200

    Add Ex1

---
+++ 02_Unit_2/01_CBasics/02_Assignments/01_HomeWork_1/EX1_PrintSentence.c
@@ -0,0 +1,12 @@
+/**
+ * File : EX1_PrintSentence.c
+ * Author : Muhammed Ali Ibrahim Ahmed 
+ * LinkedIn: https://www.linkedin.com/in/muhammedalii9/
+ * Date : 14 January , 2024
+ * Description: Print C Programming Sentence 
+*/
+#include <stdio.h>
+void main (void)
+{
+    printf(" C Programming \n");
+}
